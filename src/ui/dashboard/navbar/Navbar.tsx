b101fd2e5c532ba3bb13fba7026706bf4bea6b69
atha <athabarokahfc@gmail.com>
2024-04-20 22:02:32 +0700

    create: Navbar UI Design

--- src/ui/dashboard/navbar/Navbar.tsx
+++ src/ui/dashboard/navbar/Navbar.tsx
@@ -1,9 +1,30 @@
 import React from 'react'
+import style from './navbar.module.css'
+import { MdNotifications, MdOutlineChat, MdPublic, MdSearch } from 'react-icons/md'
+
+const PathTitle = () => {
+  return(
+    <div>
+       <span className={style.pageTitle}>Dashboard </span> 
+        <span>&gt;</span> 
+    </div>
+  );
+}
 
 const Navbar = () => {
   return (
-    <div>
-      Navbar
+    <div className={style.container}>
+      <PathTitle/>
+
+      <div className={style.menu}>
+        <div className={style.searchBar}>
+          <MdSearch />
+          <input type="text" placeholder="Search" />
+        </div>
+          <MdOutlineChat/>
+          <MdNotifications/>
+          <MdPublic/>
+      </div>
     </div>
   )
 }
--- src/ui/dashboard/righbar/RightBar.tsx
+++ src/ui/dashboard/righbar/RightBar.tsx
@@ -1,16 +1,23 @@
 import React from 'react'
 import style from './RIghtBar.module.css'
 import Image  from 'next/image';
+import { MdPlayCircleFilled } from 'react-icons/md';
 
 const SideCard = () => {
     return (
         <div className={style.SideCardItem}>
             <div className={style.bgContainer}>
-                <Image src="/images/astronaut.png" alt="astronaut"  height={100} width={100}  />
+                <Image src="/images/astronaut.png" alt="astronaut"  fill />
             </div>
             <div className={style.content}>
                 <span>🔥 Available Now</span>
-                
+                <h3>How to use new version of the admin dashboard?</h3>
+                <p>Takes 4 minutes to learn</p>
+                <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Aspernatur, distinctio.</p>
+                <button className={style.playButton}>
+                    <MdPlayCircleFilled/>
+                    Watch
+                </button>
             </div>
             
         </div>
@@ -21,6 +28,7 @@ const SideCard = () => {
 const RightBar = () => {
   return (
     <div className={style.container}>
+        <SideCard />
         <SideCard />
     </div>
   )
