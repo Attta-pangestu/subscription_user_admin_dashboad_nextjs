428748bc2e4b2c8ea788e20663bd98a0bebe8b66
atha <athabarokahfc@gmail.com>
2024-04-21 12:55:00 +0700

    create: transaction UI Card

--- src/views/dashboard/DashboardViews.tsx
+++ src/views/dashboard/DashboardViews.tsx
@@ -2,13 +2,46 @@ import DashboardLayout from '@/layout/DashboardLayout'
 import RightBar from '@/ui/dashboard/righbar/RightBar'
 import React from 'react'
 import style from './DashboardViews.module.css'
+import { MdSupervisedUserCircle } from 'react-icons/md'
+
+
+const Card = ({children, className } : {children: React.ReactNode, className?: string}) => {
+  return (
+    <div className={`${style.cardComp} ${className}`}>
+        {children}
+    </div>
+  );
+}
+
+const TransactionCard = () => {
+  return(
+    <Card className={style.transactionCard}>
+        <MdSupervisedUserCircle size={24}  />
+      <div className={style.transactionContent}>
+          <h3>Total Users</h3>
+          <span>10.534</span>
+        <p><span>10%</span> more than last month</p>
+      </div>
+    </Card>
+  );
+}
+
+const TransactionSection = () => {
+  return(
+    <section className={style.transactionSection}>
+        <TransactionCard />
+        <TransactionCard />
+        <TransactionCard />
+    </section>
+  );
+}
 
 const DashboardViews = () => {
   return (
     <DashboardLayout>
       <div className={style.container}>
           <div className={style.mainDashboard}>
-                Dashboard Views
+                <TransactionSection />
           </div>
           <div className={style.rightBar}>
             <RightBar />
